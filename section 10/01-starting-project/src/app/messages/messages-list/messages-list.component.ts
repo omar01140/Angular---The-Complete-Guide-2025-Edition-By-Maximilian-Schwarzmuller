b473682b7a4d1fc829b19ab18522f2551e408967
Omar Eltantawy <110872105+omar01140@users.noreply.github.com>
2025-02-09 11:57:10 +0200

    section 10

---
+++ section 10/01-starting-project/src/app/messages/messages-list/messages-list.component.ts
@@ -0,0 +1,23 @@
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, inject, OnInit } from '@angular/core';
+
+import { MessagesService } from '../messages.service';
+import { AsyncPipe } from '@angular/common';
+
+@Component({
+  selector: 'app-messages-list',
+  standalone: true,
+  imports:[AsyncPipe],
+  templateUrl: './messages-list.component.html',
+  styleUrl: './messages-list.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class MessagesListComponent {
+  private messagesService = inject(MessagesService)
+  messages$ = this.messagesService.messages$
+
+
+  get debugOutput() {
+    console.log('[MessagesList] "debugOutput" binding re-evaluated.');
+    return 'MessagesList Component Debug Output';
+  }
+}
